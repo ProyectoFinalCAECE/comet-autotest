367690023ae04880fcb46f8ba18f962334fcaf6e
Micha <michalski.diego@gmail.com>
2015-09-21 22:53:47 -0300

    Se arregla el test de "login" para utilizar los tags data-view Se eliminan archivos varios

--- login/login_ok.js
+++ login/login_ok.js
@@ -8,18 +8,21 @@
 module.exports = {
   'Llegar a Login' : function(client) {
     client
+      //corroboro que este en el home (no loggeado)
       .url('http://localhost:4000')
-      .waitForElementPresent('.navbar', 1000)
-      .click(".navbar-nav li:nth-child(3) a") //Boton Login
-      .assert.urlContains('account/login');
+      .waitForElementPresent('[data-view="home"]', 1000)
+      //espero que cargue la barra antes de hacerle click...
+      .waitForElementVisible('.navbar', 1000)
+      //Boton Login
+      .click(".navbar li:nth-child(2) a")
+      .waitForElementPresent('[data-view="login"]', 1000);
   },
 
   'Llegar a Dashboard' : function(client) {
     client
       .setValue('input[name=email]', ['test@test.com', client.Keys.ENTER])
       .setValue('input[name=password]', ['Test123', client.Keys.ENTER])
-      .waitForElementPresent('body', 1000)
-      .assert.urlContains('dashboard')
+      .waitForElementPresent('[data-view="dashboard"]', 1000)
       .end();
   }
 };
