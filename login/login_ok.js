9a29a892fa3128a6ea71fbb0ec70109d9eb11c04
Micha <michalski.diego@gmail.com>
2015-09-22 21:24:57 -0300

    Agregado test de creacion exitosa de proyecto

--- login/login_ok.js
+++ login/login_ok.js
@@ -1,7 +1,7 @@
 /**
  *
  * Login exitoso de usuario "test@test.com" pass "Test123"
- * Nota: De no existir, crear este usuario con estos datos exactos!!!
+ * Nota: De no existir, crear este usuario con este password!!!
  *
  */
 
@@ -11,8 +11,10 @@ module.exports = {
       //corroboro que este en el home (no loggeado)
       .url('http://localhost:4000')
       .waitForElementPresent('[data-view="home"]', 1000)
+
       //espero que cargue la barra antes de hacerle click...
       .waitForElementVisible('.navbar', 1000)
+
       //Boton Login
       .click(".navbar li:nth-child(2) a")
       .waitForElementPresent('[data-view="login"]', 1000);
@@ -20,8 +22,12 @@ module.exports = {
 
   'Llegar a Dashboard' : function(client) {
     client
-      .setValue('input[name=email]', ['test@test.com', client.Keys.ENTER])
-      .setValue('input[name=password]', ['Test123', client.Keys.ENTER])
+      //ingreso datos de login
+      .setValue('input[name=email]', ['test@test.com'])
+      .setValue('input[name=password]', ['Test123'])
+
+      //presiono "Ingresar"
+      .setValue('[type=submit]',[client.Keys.ENTER])
       .waitForElementPresent('[data-view="dashboard"]', 1000)
       .end();
   }
---
+++ project/project_create.js
@@ -0,0 +1,64 @@
+/**
+ *
+ * Creacion de un nuevo proyecto
+ * Requiere del usuario Test, clave Test123
+ *
+ */
+
+//Genero un nombre de proyecto unico
+var nombreProyecto = 'TestProject_' + new Date().toISOString();
+
+module.exports = {
+  'Llegar a Login' : function(client) {
+    client
+      //corroboro que este en el home (no loggeado)
+      .url('http://localhost:4000')
+      .waitForElementPresent('[data-view="home"]', 1000)
+
+      //espero que cargue la barra antes de hacerle click...
+      .waitForElementVisible('.navbar', 1000)
+
+      //Boton Login
+      .click(".navbar li:nth-child(2) a")
+      .waitForElementPresent('[data-view="login"]', 1000);
+  },
+
+  'Llegar a Dashboard' : function(client) {
+    client
+      //ingreso datos de login
+      .setValue('input[name=email]', ['test@test.com'])
+      .setValue('input[name=password]', ['Test123'])
+
+      //presiono "Ingresar"
+      .setValue('[type=submit]',[client.Keys.ENTER])
+      .waitForElementPresent('[data-view="dashboard"]', 1000)
+  },
+
+  'Crear Proyecto' : function(client) {
+    client
+      //corroboro que estoy en la lista de proyectos del dashboard
+      .waitForElementPresent('[data-view=project-list]', 1000)
+
+      //presiono "Crear Proyecto"
+      .setValue('[name=btnCrearProyecto]', [client.Keys.ENTER])
+
+      //corroboro que llegue a la creacion de proyecto
+      .waitForElementPresent('[data-view=project-create]', 1000)
+
+      //completo datos del proyecto
+      .setValue('[name=name]', [nombreProyecto])
+      .setValue('textarea',['Proyecto creado automáticamete por nightwatch'])
+
+      //creo el proyecto
+      .setValue('[type=submit]',[client.Keys.ENTER])
+
+      //corroboro que aparezca el de proyecto creado y lo acepto
+      .waitForElementPresent('.modal-content [type=button]', 1000)
+      .setValue('.modal-content [type=button]',[client.Keys.ENTER])
+
+      //por ultimo confirmo que vuelva a la lista de proyectos
+      .waitForElementPresent('[data-view=project-list]', 1000)
+
+      .end();
+  }
+};
